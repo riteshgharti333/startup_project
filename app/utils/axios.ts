b8cbd1cd439f680a51ef655001c833ad78368cb7
hp <riteshgharti121232@gmail.com>
2026-07-06 09:40:11 +0530

    change env

--- app/utils/axios.ts
+++ app/utils/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000",
   timeout: 30000,
   headers: {
     "Content-Type": "multipart/form-data",
